735c4d93390a6f4233e87863ef1e13ecfcc096ff
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-12-01 06:41:44 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main.component';
+import { SearchComponent } from './components/search.component';
+import { SearchlistComponent } from './components/searchlist.component';
+import { ResultsComponent } from './components/results.component';
+
+const ROUTES: Routes = [
+  { path:'', component: MainComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SearchComponent,
+    SearchlistComponent,
+    ResultsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
