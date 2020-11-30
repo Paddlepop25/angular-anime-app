68196df6f1dae4f95c43ef28ee7945d330e0421e
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-12-01 07:59:04 +0800

    added form to search page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main.component';
@@ -37,6 +38,7 @@ const ROUTES: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule, ReactiveFormsModule,
     RouterModule.forRoot(ROUTES),
     LottieModule.forRoot({ player: playerFactory })
   ],
