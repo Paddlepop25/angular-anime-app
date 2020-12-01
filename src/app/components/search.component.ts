0463c57be8fe54c6ba42d5b0488e71f0ae5f81ae
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-12-01 08:22:26 +0800

    create animeform as FormGroup and set formControlName for q in html

--- src/app/components/search.component.ts
+++ src/app/components/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -8,18 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class SearchComponent implements OnInit {
 
   genre = 'anime' // default
+  animeform: FormGroup
 
-  constructor() {
+  constructor(private fb: FormBuilder) {}
 
-   }
-
-  // create
+  // on init, create empty form, set q as formControlName
   ngOnInit(): void {
+    this.animeform = this.fb.group({
+      q: this.fb.control('', [ Validators.required ])
+    })
   }
 
   // runs when user select either anime or manga button
   setGenre(g: string) {
     this.genre = g;
-    console.info('genre chosen ---> ', this.genre)
+    // console.info('genre chosen ---> ', this.genre)
   }
 }
