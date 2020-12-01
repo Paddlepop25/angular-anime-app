2d25b5327c032b1141b320f56e4e24db4acd95e9
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-12-01 11:15:50 +0800

    managed to display all anime titles from the results

--- src/app/components/results.component.ts
+++ src/app/components/results.component.ts
@@ -11,6 +11,7 @@ export class ResultsComponent implements OnInit {
 
   genre = ''
   q = ''
+  animeResults: any[] = []
 
   constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
 
@@ -30,7 +31,25 @@ export class ResultsComponent implements OnInit {
 
     this.http.get<any>(url, { params: animeParams })
       .toPromise()
-      .then(animeResults => console.log(animeResults['results']))
-      .catch((error: HttpErrorResponse) => { console.log('HttpError:', error )})
-
+      .then(animeResults => {
+        this.animeResults = animeResults['results']
+        console.log('this.animeResults ---> ', this.animeResults)
+      }
+      )
+      .catch((error: HttpErrorResponse) => { console.log('HttpError ---> ', error )})
 }}
+
+// One anime result:
+// airing: false
+// end_date: "2011-07-22T00:00:00+00:00"
+// episodes: 2
+// image_url: "https://cdn.myanimelist.net/images/anime/10/74217.jpg?s=d1002e34fd5b0d1c0d86ac45a39ee47e"
+// mal_id: 30840
+// members: 697
+// rated: "PG-13"
+// score: 5.33
+// start_date: "2011-01-22T00:00:00+00:00"
+// synopsis: "Specials included on SD Gundam Collection Box."
+// title: "Many Things SD Gundam"
+// type: "Special"
+// url: "https://myanimelist.net/anime/30840/Many_Things_SD_Gundam"
--- src/app/models.ts
+++ src/app/models.ts
@@ -8,3 +8,24 @@ export interface SearchOption {
   genre: Genre
 }
 
+export interface animeResult {
+  image_url: string,
+  rated: string,
+  sypnosis: string,
+  title: string,
+  url: string
+}
+
+// airing: false
+// end_date: "2011-07-22T00:00:00+00:00"
+// episodes: 2
+// image_url: "https://cdn.myanimelist.net/images/anime/10/74217.jpg?s=d1002e34fd5b0d1c0d86ac45a39ee47e"
+// mal_id: 30840
+// members: 697
+// rated: "PG-13"
+// score: 5.33
+// start_date: "2011-01-22T00:00:00+00:00"
+// synopsis: "Specials included on SD Gundam Collection Box."
+// title: "Many Things SD Gundam"
+// type: "Special"
+// url: "https://myanimelist.net/anime/30840/Many_Things_SD_Gundam"
